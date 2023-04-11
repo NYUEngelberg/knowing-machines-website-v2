8020aaf7c5c318004b0877e28fecdf66fcaaeb97
mltn <milutinmarinovic@gmail.com>
2023-04-11 11:53:37 +0200

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '@/styles/globals.css'
-import '@/styles/fonts.css'
-import '@/styles/markdown-content.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import "@/styles/fonts.css";
+import "@/styles/markdown-content.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-5D2YJXSXPP"
+      ></Script>
+      <Script strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){
+          dataLayer.push(arguments);
+        }
+        gtag('js', new Date()); gtag('config', 'G-5D2YJXSXPP');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
