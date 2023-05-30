47091413ce3b8bf035c55a82536e2bb74b12a600
mltn <milutinmarinovic@gmail.com>
2023-05-30 12:54:00 +0200

    Update [publication].tsx

--- pages/publications/[publication].tsx
+++ pages/publications/[publication].tsx
@@ -1,3 +1,4 @@
+import Layout from "@/components/Layout";
 import { PublicationMetaData } from "@/types/publications";
 import { getPublicationByHref, getPublicationPagePaths } from "@/util/publications";
 
@@ -6,11 +7,19 @@ type Props = {
 }
 
 export default function PublicationPage({publication}:Props) {
-    return <div>
+    return <Layout title={"Knowing Machines"} navbarDefaultCollapsed={false}>
         <div
             className="border-[1px] border-black p-6 mb-6 flex flex-col items-center gap-[40px]"
           >
-            <img src={publication.coverImg} alt={publication.coverImgAlt} />
+            <div className="relative my-12 p-6 border-black border-[1px] border-b-0">
+                <img src={publication.coverImg} alt={publication.coverImgAlt} />
+                <div className="absolute top-[-4px] right-[-4px] h-[4px] w-[250px] bg-black"></div>
+                <div className="absolute top-[-4px] right-[-4px] h-[100px] w-[4px] bg-black"></div>
+                <div className="absolute bottom-0 left-0 h-[4px] w-[100px] bg-black"></div>
+                <span className="absolute -top-6 left-[-1px] content-type text-xs text-white uppercase bg-black p-2 py-1 self-start">
+                    {publication.contentType}
+                </span>
+            </div>
             <div
               id="set-collection-items"
               className="relative border-black border-t-[1px]"
@@ -43,7 +52,8 @@ export default function PublicationPage({publication}:Props) {
               </div>
             </div>
           </div>
-    </div>
+          <></>
+    </Layout>
 };
 
 export async function getStaticPaths() {
