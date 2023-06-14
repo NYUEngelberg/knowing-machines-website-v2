46c723de1e51d99c817ed7364ce1fc764f9c223e
mltn <milutinmarinovic@gmail.com>
2023-06-14 11:46:29 +0200

    added plausible.io analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,25 +2,12 @@ import "@/styles/globals.css";
 import "@/styles/fonts.css";
 import "@/styles/markdown-content.css";
 import type { AppProps } from "next/app";
-import Script from "next/script";
+import PlausibleProvider from "next-plausible";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-5D2YJXSXPP"
-      ></Script>
-      <Script strategy="afterInteractive">
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){
-          dataLayer.push(arguments);
-        }
-        gtag('js', new Date()); gtag('config', 'G-5D2YJXSXPP');
-        `}
-      </Script>
+    <PlausibleProvider domain="knowingmachines.org">
       <Component {...pageProps} />
-    </>
+    </PlausibleProvider>
   );
 }
