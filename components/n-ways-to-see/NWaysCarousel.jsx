af9e449ced30a15e2af061b51b62f7883281ba00
mltn <milutinmarinovic@gmail.com>
2023-06-30 13:48:49 +0200

    Widget carousel - reduced width

--- components/n-ways-to-see/NWaysCarousel.jsx
+++ components/n-ways-to-see/NWaysCarousel.jsx
@@ -70,7 +70,7 @@ export default function NWaysCarousel({ title, collection, apiURL }) {
   }
   return (
     <>
-      <div className="w-full border self-center border-black relative aspect-video duration-400">
+      <div className="max-w-xl mx-auto w-full border self-center border-black relative aspect-video duration-400">
         {item && <NWaysCarouselImage item={item} key={item.id} />}
       </div>
       <NWaysPagination
