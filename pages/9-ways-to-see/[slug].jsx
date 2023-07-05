9401ba8dee089f301888f213b9815ce7c02999da
killapop <killapop@protonmail.com>
2023-07-05 13:41:50 +0000

    remove sup links from footnotes

--- pages/9-ways-to-see/[slug].jsx
+++ pages/9-ways-to-see/[slug].jsx
@@ -117,11 +117,15 @@ export default function NWaysPage({ content, frontmatter, publication }) {
         }
       });
       setHtmlOutput(output);
-      if (footnotes.children.length > 0) {
+      const filteredFootnotes = filter(
+        toHast(footnotes),
+        (node) => node.tagName !== "sup"
+      );
+      if (filteredFootnotes.children.length > 0) {
         sections.push({
           type: "paragraph",
           tagName: "div",
-          content: toHtml(toHast(footnotes)),
+          content: toHtml(filteredFootnotes),
         });
       }
       return setSections(sections);
@@ -256,6 +260,9 @@ export default function NWaysPage({ content, frontmatter, publication }) {
             width: 100%;
             height: auto;
             margin-bottom: 1em;
+          }
+          .footnotes {
+            margin-top: 4em;
           }
         `}</style>
         <CollectionLinks publication={publication} />
