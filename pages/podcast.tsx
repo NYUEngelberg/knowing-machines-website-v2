cf08fd36bce3dea4dd28a28595d26f7b5f34eeaa
mltn <milutinmarinovic@gmail.com>
2023-10-31 10:32:40 +0100

    Update podcast.tsx

--- pages/podcast.tsx
+++ pages/podcast.tsx
@@ -26,27 +26,26 @@ export default function PodcastPage({}: Props) {
           <HomePageHeading text={"Podcast"} />
           <div className={"pb-10 md:p-10 relative"}>
             <div className="absolute top-0 left-0 w-[1px] h-[333px] bg-black"></div>
-            
-            <iframe
-              className="border-black border-[1px]"
-              height="200px"
-              width="100%"
-              frameBorder={"0"}
-              scrolling="no"
-              seamless
-              src="https://player.simplecast.com/90a7d53e-86d4-46ec-8029-cb85691aed40?dark=false"
-            ></iframe>
-            <br></br>
-            <iframe
-              className="border-black border-[1px]"
-              height="200px"
-              width="100%"
-              frameBorder={"0"}
-              scrolling="no"
-              seamless
-              src="https://player.simplecast.com/265b9a8a-d0c3-4d50-a239-af1dc0f2b67d?dark=false"
-            ></iframe>
-            
+            <div className="flex flex-col gap-6">
+              <iframe
+                className="border-black border-[1px]"
+                height="200px"
+                width="100%"
+                frameBorder={"0"}
+                scrolling="no"
+                seamless
+                src="https://player.simplecast.com/265b9a8a-d0c3-4d50-a239-af1dc0f2b67d?dark=false"
+              ></iframe>
+              <iframe
+                className="border-black border-[1px]"
+                height="200px"
+                width="100%"
+                frameBorder={"0"}
+                scrolling="no"
+                seamless
+                src="https://player.simplecast.com/90a7d53e-86d4-46ec-8029-cb85691aed40?dark=false"
+              ></iframe>
+            </div>
           </div>
         </section>
       </main>
