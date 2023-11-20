58a663819e02b6618f3e608df29f89c3dd7488fd
Michael Weinberg <mwweinberg@users.noreply.github.com>
2023-11-20 07:15:12 -0500

    Update podcast.tsx

--- pages/podcast.tsx
+++ pages/podcast.tsx
@@ -66,6 +66,15 @@ export default function PodcastPage({}: Props) {
                 seamless
                 src="https://player.simplecast.com/da7d84a6-ef94-44cb-a700-5b88f0786904?dark=false"
               ></iframe>
+              <iframe
+                className="border-black border-[1px]"
+                height="200px"
+                width="100%"
+                frameBorder={"0"}
+                scrolling="no"
+                seamless
+                src="https://player.simplecast.com/3e822441-3c3b-459e-a55f-ce9a50e9828d?dark=false"
+              ></iframe>
               
               
             </div>
